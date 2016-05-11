3d85918b482ac5f45f68d0e30769667e435a8c26
Natan Camargos <natan.camargos@ifactory.com.br>
2016-05-11 17:03:40 -0300

    Gulp postinstall fix;

--- build/tasks/postinstall.js
+++ build/tasks/postinstall.js
@@ -2,16 +2,16 @@ var q = require('q');
 var child_process = require('child_process');
 var path = require('path');
 
-var exec = q.nbind(child_process.exec, child_process);
+var execSync = q.nbind(child_process.execSync, child_process);
 var rootDir = path.join(__dirname, '../..');
 
 module.exports = function (gulp) {
 	gulp.task('postinstall', function (cb) {
 		q.all([
-			exec('typings install', { cwd: path.join(rootDir, 'app') }),
-			exec('typings install', { cwd: path.join(rootDir, 'server') }),
-			exec('bower install', { cwd: rootDir })
+			execSync('typings install', { cwd: path.join(rootDir, 'app') }),
+			execSync('typings install', { cwd: path.join(rootDir, 'server') }),
+			execSync('bower install', { cwd: rootDir })
 		])
-		.nodeify(cb);
+		.finally(cb);
 	});
 };
