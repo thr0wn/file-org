5dca5580ec54db48dd335f5af34406cd2ed1f506
Natan Camargos <natan.camargos@ifactory.com.br>
2016-05-19 14:03:00 -0300

    Using ES6 const;

--- server/src/config/constants.ts
+++ server/src/config/constants.ts
@@ -3,11 +3,11 @@ process.env.NODE_CONFIG_DIR = path.join(__dirname, '../../../build/properties');
 import * as config from 'config';
 
 // files and directories
-export let SERVER_STATIC_DIR = config.get<string>('server.static.directories');
-export let UPLOAD_DIR = path.join(__dirname, '../../files');
-export let LOG_FILE = path.join(__dirname, '../../logs/main.log');
-export let SESSION_SECRET = 'lHCBrJg6zlTluLLPaObMcEZMcqDt6LCAamG1ByOWcN1cjAKsL0iULKpGA6PqPXV';
-export let SERVER_PORT = config.get<string>('server.port');
+export const SERVER_STATIC_DIR = config.get<string>('server.static.directories');
+export const UPLOAD_DIR = path.join(__dirname, '../../files');
+export const LOG_FILE = path.join(__dirname, '../../logs/main.log');
+export const SESSION_SECRET = 'lHCBrJg6zlTluLLPaObMcEZMcqDt6LCAamG1ByOWcN1cjAKsL0iULKpGA6PqPXV';
+export const SERVER_PORT = config.get<string>('server.port');
 
 // database
-export let DATABASE_URI = config.get<string>('db.uri');
+export const DATABASE_URI = config.get<string>('db.uri');
